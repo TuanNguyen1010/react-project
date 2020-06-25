c0b2ec1175635490e790cf9bc90cc64c2af0def1
Tuan <Tuan.viet.nguyen@hotmail.co.uk>
2020-06-25 20:20:09 +0100

    Chore: remove console.log

--- src/App.js
+++ src/App.js
@@ -11,7 +11,6 @@ class App extends Component {
   }
 
   changeNames = () => {
-    console.log("working")
     this.setState({persons: [
       {name: "Daniel", hobbies:'golf'},
       { name: 'Johnny', hobbies: 'tennis'}
