dc8c8fc3ab56615d9a6b3b212774de59245b3257
Tuan <Tuan.viet.nguyen@hotmail.co.uk>
2020-06-27 23:59:12 +0100

    Style: connect Person with inline style

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,8 +1,15 @@
-import React from 'react'
+import React, {useState} from 'react'
+import './Person.css'
 
 const person = (props) => {
+  const [ spareState, setSpareState] = useState({
+    default: "no name"
+  })
   return (
+    <div> 
     <p> My name is {props.name} and I like {props.hobbies} </p>
+    <p className="Default-tag"> spare name: {spareState.default}</p>
+    </div>
   )
 }
 
