fbd2ac7258c9ba18363dca7568a4647949a18ae2
Tuan <Tuan.viet.nguyen@hotmail.co.uk>
2020-06-28 14:44:32 +0100

    Chore: dynamic arguement for changeNames

--- src/App.js
+++ src/App.js
@@ -10,10 +10,10 @@ class App extends Component {
     ]
   }
 
-  changeNames = () => {
+  changeNames = (fullname) => {
     this.setState({persons: [
-      {name: "Daniel", hobbies:'golf'},
-      { name: 'Johnny', hobbies: 'tennis'}
+      {name: fullname, hobbies:'golf'},
+      { name: 'John', hobbies: 'tennis'}
     ]
     })
   }
@@ -23,7 +23,7 @@ class App extends Component {
         <header className="App-header">
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <button onClick={this.changeNames}> Switch Name</button>
+        <button onClick={() => {this.changeNames("Daniel")}}> Switch Name</button>
         <Person 
           name={this.state.persons[0].name} 
           hobbies={this.state.persons[0].hobbies}/>
