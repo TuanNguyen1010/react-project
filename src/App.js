0af7b43c9f9f16a61a01fac2b9b5d63ba363f468
Tuan <Tuan.viet.nguyen@hotmail.co.uk>
2020-06-28 15:52:14 +0100

    Chore: change render to dynamic

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ class App extends Component {
       { name: 'Danny', hobbies:'golf'},
       { name: 'John', hobbies: 'tennis'}
     ],
-    showPersons: false
+    showPersons: true
   }
 
   changeNames = (fullname) => {
@@ -19,24 +19,35 @@ class App extends Component {
     })
   }
 
-  togglePersonName = () => {
-
+  togglePersonState = () => {
+    console.log("button pressed")
+    const personState = this.state.showPersons
+    this.setState({showPersons: !personState})
   }
 
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <button onClick={() => {this.changeNames("Daniel")}}> Switch Name</button>
+
+    let persons = null;
+    if (this.state.showPersons) {
+    persons = (
+      <div>
         <Person 
           name={this.state.persons[0].name} 
           hobbies={this.state.persons[0].hobbies}/>
         <Person 
         name={this.state.persons[1].name} 
         hobbies={this.state.persons[1].hobbies}
-        click={() => this.changeNames("Daniel")}/>
+        click={this.changeNames.bind(this, "Danny")}/>
+        </div>)}
+
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1 className="App-title">Welcome to React</h1>
+        </header>
+        <button onClick={this.togglePersonState}> Switch Name</button>
+        {persons}
+        
       </div>
     );
   }
